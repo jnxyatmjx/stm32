9f59dd88f9343f8f49a329df0121b177409468d2
jnxyatmjx <jnxyatmjx@qq.com>
2023-04-27 15:05:57 +0800

    add LED toggling and associate files

--- Src/main.c
+++ Src/main.c
@@ -100,7 +100,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+        HAL_GPIO_TogglePin(LED_1_GPIO_Port,LED_1_Pin);
+        HAL_Delay(90);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
